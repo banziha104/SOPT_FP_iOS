1a30993c93b2c258658e57a187a58a547039ce4f
iyeongjun <they6687@naver.com>
2018-05-04 16:55:13 +0900

    add

--- SOPT_FP/ViewController.swift
+++ SOPT_FP/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    // 0부터 500 까지 들어있는 문자열 배열에
+    // 짝수의 합을 구하라
+    
+    var list : [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        for i in 0...500 { list.append("\(i)")}
+        
+        var resultList : [Int] = []
+        for i in list{
+            let temp = Int(i)
+            if(temp! % 2 == 0){
+                resultList.append(temp!)
+            }
+        }
+        
+        var result = 0
+        for i in resultList{
+            result += i
+        }
+        
+        print(result)
+        
+        
+        
+        let result2 =  list // iterable 객체에
+            .map { Int($0) } // [String] -> [Int]
+            .filter { $0! % 2 == 0 } // [Int] 짝수인 애들
+            .reduce(0) { (result, num) -> Int in
+                guard let num = num else{return 0}
+                return result + num
+        }
+        
+        print("test")
+        print(result2)
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
